fc8a02c6ef67c767e256af216311cc4323291ece
Your Name <you@example.com>
2020-07-27 16:10:14 -0300

    Programas executados no JS, aula 9

---
+++ semana3/variaveis/js-vanilla-template/src/index.js
@@ -0,0 +1,66 @@
+// a = 10
+// b = 10
+//     //vai imprimir o valor de (b) que é 10 na tela
+// console.log(b)
+
+// b = 5
+//     //vai imprimir o valor de (a) que é 10 e o valor de (b) que agora é 5
+// console.log(a, b)
+
+// a = 10
+// b = 20
+// c = a
+// b = c
+// a = b
+//     //vai imprimir o valor de (a) que é 10, valor de (b) que é 10 e valor de (c) que é 10
+// console.log(a, b, c)
+
+// let nome
+// let idade
+
+// console.log(nome, idade)
+// não funcionou pois não tem nenhum valor atribuido a variavel nome e idade
+
+// let nome = prompt("Qual o seu nome?")
+// let idade = prompt("Qual a sua idade?")
+
+// console.log(typeof nome, typeof idade)
+//     // apareceu que os typeofs são strings pois no idade tem uma frase dentro dos parenteses em vez de um número.
+// console.log('Ola', nome, 'voce tem', miidade, 'anos')
+
+// let nome = prompt("Qual seu nome?")
+// console.log("1.Qual seu nome? Resposta:", nome)
+// let peso = prompt("Qual seu peso?")
+// console.log("2.Qual o seu peso? Resposta:", peso)
+// let endereço = prompt("Qual seu endereço?")
+// console.log("3.Qual o seu endereço? Resposta:", endereço)
+// let estadoCivil = prompt("Qual seu estado civil?")
+// console.log("4.Qual o seu estado Civil? Resposta:", estadoCivil)
+// let etnia = prompt("Qual sua etnia?")
+// console.log("5.Qual a sua etnia? Resposta:", etnia)
+
+
+// let array = ['pizza', 'xis', 'lasanha', 'strogonoff', 'sushi']
+// console.log(array)
+// console.log("essas são as minhas comidas preferidas")
+// console.log(array[0])
+// console.log(array[1])
+// console.log(array[2])
+// console.log(array[3])
+// console.log(array[4])
+
+// let comida = prompt("Qual sua comida preferida?")
+// let array = ['pizza', comida, 'lasanha', 'strogonoff', 'sushi']
+// console.log(array)
+// console.log("essas são as minhas comidas preferidas")
+// console.log(array[0])
+// console.log(array[1])
+// console.log(array[2])
+// console.log(array[3])
+// console.log(array[4])
+
+// let perguntas = ["Voce é mulher?", "Você está vestindo casaco ou moletom?", "Você está vestindo blusa branca?"]
+// let respostas = [true, true, false]
+// console.log(perguntas[0], respostas[0])
+// console.log(perguntas[1], respostas[1])
+// console.log(perguntas[2], respostas[2])
