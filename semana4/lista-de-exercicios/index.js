7ed0d29ea816131286c2e3d76255cb5cb557fcf9
Your Name <you@example.com>
2020-08-07 12:18:29 -0300

    Exercicios de interpretação de códigos completo

---
+++ semana4/lista-de-exercicios/index.js
@@ -0,0 +1,35 @@
+// EXERCICIOS DE INTERPRETAÇÃO DE TEXTO
+
+//-------------------------1---------------------------------
+
+/* O código transfere o valor em dinheiro no valor da cotação do dolar informado pelo usuario pelo prompt
+o usuario digita o valor da cotação e ele pega esse valor e faz vezes o valor em reais informado
+no código quando chama a função em baixo, esse valor ele guarda numa constante ''meuDinheiro" e
+depois da um console.log na constante imprimindo o valor de 100 dolares vezes a cotação.*/
+
+//-------------------------2---------------------------------
+
+/* A função ele tem como parametro o tipo de investimento e o valor, depois ele abre um switch case pra avaliar
+cada caso pra calcular o valor de juros do investimento dependendo do tipo dele, caso o investimento não se 
+enquadra nos tipos do switch o usuario recebe um alert informando, no final ele retorna esse valor e guarda numa variavel
+então ele chama a função passando valores de paramentro e da um console.log pra imprimir na tela
+impresso na tela : 1ª (165,00) e 2ª um alert informando "TIPO DE INVESTIMENTO INFORMADO INCORRETO"*/
+
+//-------------------------3---------------------------------
+
+/* Primeiro o código cria 3 tipos de array, um com numeros e os outros dois vazios, logo a baixo ele cria um laço de
+for of onde vai varrer todo o array de numeros e se o numero a divisão dele por 2 o resto for igual a 0 ele adiciona
+o numero no array1, caso ao contrario ele adiciona o array2. Depois da um console log no tamanho dos 3 arrays.
+impresso tela: 1ª (Quantidade total de números 14 ) 
+2ª (6)
+3ª(8)*/
+
+//-------------------------4---------------------------------
+
+/* primeiro o código é um array composto de numeros inteiros e negativos, abaixo duas variaveis, uma guarda o valor de
+infinito e outra o valor de 0, abaixo um laço de for of que varre o array para encaixar na condição dos if's, 
+o primeiro if diz que o numero do array for menor que infinito a variavel vai possuir aquele valor, e o segundo if
+pede que o numero seja maior que 0 ele atribui esse valor, isso é feito pra achar o maior e o menor valor do array de numeros
+logo um console log nessas variaveis 
+imprime na tela: 1ª (1590) 2ª(-10)
+
