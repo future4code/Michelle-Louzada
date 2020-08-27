4b0442e1f208a027346f309d1e1c08eb4e93ae1f
Michelle Louzada <hinatah.louz@gmail.com>
2020-08-27 16:46:16 -0300

    Inserção de fotos

--- semana7/integracoes-api-publica/src/App.js
+++ semana7/integracoes-api-publica/src/App.js
@@ -9,11 +9,11 @@ const InputPersonalizade = styled.input`
   box-shadow: 0px 5px 5px -2px rgba(0,0,0,0.25);
   border: none;
   width: ${props => props.width};
-  height: 35px;
+  height: 40px;
   outline: none;
   margin: 10px 2px 10px 3px;
   border-radius: 5px;
-  width: 180px;
+  width: 220px;
     
 `
 const ButtonPersonalizade = styled.button`
@@ -37,7 +37,7 @@ const ButtonPersonalizade = styled.button`
 `
 const All = styled.div`
   width: 500px;
-  height: 350px;
+  height: 650px;
   background-color: #FFE4C4;
   text-align: center;
   border: 2px solid black;
@@ -49,11 +49,16 @@ const All = styled.div`
 const Titulo = styled.h1 `
   margin: 10px;
   padding: 5px;
+  font-size: 50px;
 `
 const Container = styled.div`
  display: flex;
  justify-content: center;
   
+`
+const Image = styled.img`
+  width: 80%;
+  height: 200px;
 `
 export default class App extends React.Component {
   state = {
@@ -80,14 +85,32 @@ export default class App extends React.Component {
   onChangeName = (event) => {
     this.setState({name: event.target.value})
   }
+  renderImage = () => {
+    if (this.state.age <= 5){
+      return <Image src="https://cdn.maemequer.pt/wp-content/uploads/2015/08/bebe-375.jpg" />
+    }else if (this.state.age <= 15){
+      return <Image src="https://cdn.maemequer.pt/wp-content/uploads/2015/09/menina-500.jpg" />
+    }else if (this.state.age <= 25){
+      return <Image src="https://mercado.etc.br/wp-content/uploads/2016/08/1470356496_379_Lembra-da-crian%C3%A7a-mais-linda-do-mundo-Ela-cresceu-e-est%C3%A1-maravilhosa.jpg" />
+    }else if(this.state.age <= 40){
+      return <Image src="https://www.sbd-sp.org.br/geral/wp-content/uploads/2017/10/25anos.jpg" />
+    }else if(this.state.age <= 60){
+      return <Image src="https://ccolhos.com.br/wp-content/uploads/2015/06/post03.jpg" />
+    }else if(this.state.age <= 80){
+      return <Image src="https://3.bp.blogspot.com/-_hPkWbtLNXg/UemdZ5BfqBI/AAAAAAAAElM/Ye_P1hRG1GY/w1200-h630-p-k-no-nu/Idosos.jpg" />
+    }else if(this.state.age <= 100){
+      return <Image src="https://2.bp.blogspot.com/-zywteJV6N9E/VusEKoGolRI/AAAAAAAAaHo/-rjC2YFjqNEVLvjlOQZTCqV8mPlzonerQ/s1600/7fa6c9bff71c6f91be169c624da965c8.jpg" />
+    }
+  }
   render(){
     return(
       <Container>
         <All>
-          <Titulo>Vamos Advinhar Sua Idade Pelo Seu Nome?</Titulo>
+          <Titulo>Vamos advinhar Sua Idade Pelo Seu Nome?</Titulo>
           <InputPersonalizade placeholder="Seu nome <3" onChange={this.onChangeName} />
           <ButtonPersonalizade onClick={() => this.playNames(this.state.name)}>Enviar!</ButtonPersonalizade>
-          <h3>Sua Idade: {this.state.age}</h3>
+          <h1>Sua Idade: {this.state.age}</h1>
+          {this.renderImage()}
         </All>
       </Container>
     )
