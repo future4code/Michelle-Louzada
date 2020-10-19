a12599ab4d88eb29bdcb3bc57ac08038cbf52d85
Michelle Louzada <hinatah.louz@gmail.com>
2020-10-19 17:35:43 -0300

    atividades completas com algumas verificações

--- semana15/node-packpage/index.js
+++ semana15/node-packpage/index.js
@@ -5,30 +5,66 @@ process.argv[1];
 // ECERCICIO 1
 
 // const name = process.argv[2]
-// const idade = Number(process. argv[3])
+// const idade = Number(process.argv[3])
 // const soma = Number(process.argv[4])
 
 // console.log("Olá, "+name+"! Você tem "+idade+" anos. Em sete anos você terá "+(idade+soma)+"")
 
 //EXERCICIO 2
 
-// const numero1 = Number(process.argv[2])
-// const numero2 = Number(process. argv[3])
+//Soma (npm run add)
 
-// console.log("resposta: "+(numero1+numero2)+"")
+// const numero1 = Number(process.argv[4])
+// const numero2 = Number(process. argv[5])
+
+// if(isNaN(numero1) || isNaN(numero2)){
+//     console.log("INSIRA NUMEROS, POR FAVOR")
+// }else{
+//     console.log(numero1+numero2);
+// }
+
+// Subtração (npm run sub)
+
+// const numero1 = Number(process.argv[4])
+// const numero2 = Number(process. argv[5])
+
+// if(isNaN(numero1) || isNaN(numero2)){
+//     console.log("INSIRA NUMEROS, POR FAVOR")
+// }else{
+//     console.log(numero1+numero2);
+// }
+
+// Multiplicação (npm run mult)
+
+// const numero1 = Number(process.argv[4])
+// const numero2 = Number(process. argv[5])
+
+// if(isNaN(numero1) || isNaN(numero2)){
+//     console.log("INSIRA NUMEROS, POR FAVOR")
+// }else{
+//     console.log(numero1+numero2);
+// }
+
+// Divisão (npm run div)
+
+// const numero1 = Number(process.argv[4])
+// const numero2 = Number(process. argv[5])
+
+// if(isNaN(numero1) || isNaN(numero2)){
+//     console.log("INSIRA NUMEROS, POR FAVOR")
+// }else{
+//     console.log(numero1+numero2);
+// }
 
 //Exercicio 3
 
-const tarefa = process.argv[2]
+// const tarefa = process.argv[2]
+// const listaDeTarefas = [" correr", " cozinhar", " estudar"]
 
-const addTarefa = (tarefa) => {
-    const listaDeTarefas = []
-    const tarefas = [
-        {
-        tarefas: [...listaDeTarefas, tarefa]
-        }
-    ]
-    
-    return tarefas
-}
-console.log(addTarefa(tarefa))
+// const addTarefa = (tarefa) => {
+//     listaDeTarefas.push(tarefa)
+//     console.log(`tarefa adicionada com sucesso. tarefas: ${listaDeTarefas}`)
+
+// }
+
+// addTarefa(tarefa)
