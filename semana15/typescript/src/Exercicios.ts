a662bf92b4be3a3c3b895c54689eb6cf6ddf16fa
Michelle Louzada <hinatah.louz@gmail.com>
2020-10-20 16:51:28 -0300

    Atividade 1 aula de typescript

---
+++ semana15/typescript/src/Exercicios.ts
@@ -0,0 +1,10 @@
+//Exercicio 1
+
+//a) minhaString fica sublinhado e acusa que é um tipo string e numero não vai
+//funcionar;
+
+const minhaString: string = 43
+
+//b) podemos usar o | pra atribuir mais tipos a varivél, com a sintese abaixo:
+
+let meuNumero: number | string = 45
