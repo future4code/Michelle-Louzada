17667d027d3e28a9c815f5da4df63bbe2aec512a
Michelle Louzada <hinatah.louz@gmail.com>
2020-11-17 16:34:10 -0300

    começo do projeto, separação de pastas e endpoints

---
+++ semana19/labook/template-labook/src/data/user/insertUser.ts
@@ -0,0 +1,10 @@
+
+import { connection } from "../..";
+import { UserInsert } from "../../model/User";
+
+export const insertUser = async (data: UserInsert ): Promise<void> => {
+  const {id, email, cypherPassword, name } = data;
+  const password = cypherPassword
+  await connection ("labook_users")
+    .insert({id, email, password, name});
+}
